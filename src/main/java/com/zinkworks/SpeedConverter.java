caa4720ae8309300d64bd8954cd65c8bab3b68f9
Ruth Cassidy <ruth.cassidy@zinkworks.com>
2022-06-02 09:37:11 +0100

    Fix speed converter by using one method

--- src/main/java/com/zinkworks/SpeedConverter.java
+++ src/main/java/com/zinkworks/SpeedConverter.java
@@ -1,16 +1,17 @@
 package com.zinkworks;
 
 public class SpeedConverter {
-    public static long toMilesPerHour(double kilometersPerHour) {
-        if (kilometersPerHour < 0) {
-            return -1;
-        }
-
-        return Math.round(kilometersPerHour / 1.609);
-    }
+//    public static long toMilesPerHour(double kilometersPerHour) {
+//        if (kilometersPerHour < 0) {
+//            return -1;
+//        }
+//
+//        return Math.round(kilometersPerHour / 1.609);
+//    }
 
     public static void printConversion(double kilometersPerHour) {
-        long milesPerHour = toMilesPerHour(kilometersPerHour);
+        long milesPerHour = Math.round(kilometersPerHour / 1.609);
+
         if (kilometersPerHour < 0) {
             System.out.println("Invalid Value");
         } else {
