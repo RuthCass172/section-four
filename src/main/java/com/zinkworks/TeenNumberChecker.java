020dd7f84c2513cd35dad37cdec0dc0c9b3e2e6a
Ruth Cassidy <ruth.cassidy@zinkworks.com>
2022-06-02 09:40:20 +0100

    Simplify methods in teen number checker class

--- src/main/java/com/zinkworks/TeenNumberChecker.java
+++ src/main/java/com/zinkworks/TeenNumberChecker.java
@@ -2,16 +2,10 @@ package com.zinkworks;
 
 public class TeenNumberChecker {
     public static boolean hasTeen(int num1, int num2, int num3) {
-        if ((num1 <= 19 && num1 >= 13) || (num2 <= 19 && num2 >= 13) || (num3 <= 19 && num3 >= 13)) {
-            return true;
-        }
-        return false;
+        return (num1 <= 19 && num1 >= 13) || (num2 <= 19 && num2 >= 13) || (num3 <= 19 && num3 >= 13);
     }
 
     public static boolean isTeen(int num) {
-        if (num <= 19 && num >= 13) {
-            return true;
-        }
-        return false;
+        return num <= 19 && num >= 13;
     }
 }
